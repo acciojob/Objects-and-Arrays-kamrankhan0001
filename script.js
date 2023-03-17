81b97debb99883746ce66539675ea69e9ca997a0
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-03-17 18:51:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,5 +7,9 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
+const team = players;
 
+// Create a copy of the players object
+const team1 = Object.assign({}, players);
+const cap1 = Object.assign({}, person);
 export {players,person,team,team1,cap1}
